407a35d3a542368b8b997b5fa5db6440364636f5
avokeys <alexander.ayala.palacin@gmail.com>
2022-02-27 05:53:20 +0000

    trying to implement keydown to show  input when enter is pressed

--- src/js/component/UserInput.js
+++ src/js/component/UserInput.js
@@ -14,7 +14,9 @@ export const UserInput = () => {
 				placeholder="What needs to be done?"
 				onChange={handleListItem}
 			/>
+
 			<input type="text" className="item" value={listItem} />
+
 			<input type="text" className="item itemsLeft" />
 		</div>
 	);
