6d324c9db96d60e909390c9659c41c7acd5b07d9
avokeys <alexander.ayala.palacin@gmail.com>
2022-02-28 00:56:44 +0000

    input value displaying in list item, need to figure out event listener for enter

--- src/js/component/UserInput.js
+++ src/js/component/UserInput.js
@@ -5,7 +5,14 @@ export const UserInput = () => {
 	const handleListItem = (event) => {
 		setListItem(event.target.value);
 	};
-
+	const todoItem = (event) => {
+		if (event.keycode === 13) {
+			return true;
+		} else {
+			return false;
+		}
+	};
+	console.log(todoItem);
 	return (
 		<div>
 			<input
@@ -14,8 +21,14 @@ export const UserInput = () => {
 				placeholder="What needs to be done?"
 				onChange={handleListItem}
 			/>
-
-			<input type="text" className="item" value={listItem} />
+			{todoItem && (
+				<input
+					type="text"
+					className="item"
+					value={listItem}
+					onChange={handleListItem}
+				/>
+			)}
 
 			<input type="text" className="item itemsLeft" />
 		</div>
